28bb66e67b940a987c0204e1562c9702af22a418
Mohammed S Shurrab <mshurrab@extreme.sa>
2020-01-26 12:34:52 +0300

    get the list of categories, filter them by type, and sort them by usage

--- app/Category.php
+++ app/Category.php
@@ -12,4 +12,24 @@ class Category extends Model
     const DEFAULT_CATEGORY = 1;
 
     protected $guarded = [];
+
+    public static function scopeFilter($query,$filters)
+    {
+        if(isset($filters['type'])){
+            $query->where('type',$filters['type']);
+        }
+
+        return $query;
+    }
+
+    public static function scopeSort($query,$filters)
+    {
+        if(isset($filters['sort']) && $filters['sort'] == 'usage'){
+            $query->orderByDesc(
+                Transaction::selectRaw('count(*) as count')->whereColumn('category_id','categories.id')
+            );
+        }
+
+        return $query;
+    }
 }
--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -20,6 +20,36 @@ class CategoryTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function test_can_get_categories_list()
+    {
+        factory(Category::class, 5)->create();
+
+        $response = $this->get('api/categories');
+
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            [
+                'id',
+                'name',
+                'type'
+            ]
+        ]);
+    }
+
+    public function test_can_filter_categories_per_type()
+    {
+        factory(Category::class, 3)->create(['type' => Category::INCOME]);
+        factory(Category::class, 2)->create(['type' => Category::EXPENSES]);
+
+        $response = $this->get('api/categories?type=' . Category::INCOME);
+        $response->assertSuccessful();
+        $response->assertJsonCount(3);
+
+        $response = $this->get('api/categories?type=' . Category::EXPENSES);
+        $response->assertSuccessful();
+        $response->assertJsonCount(2);
+    }
+
     public function test_wallet_can_track_income_with_category()
     {
         $wallet = factory(Wallet::class)->create();
@@ -72,5 +102,32 @@ class CategoryTest extends TestCase
         $this->assertEquals('Restaurant', $transaction->note);
         $this->assertEquals(-100, $transaction->amount);
         $this->assertInstanceOf(Wallet::class, $transaction->trackable);
+    }
+
+    public function test_can_sort_categories_based_on_usage()
+    {
+        $categoriesTransactions = collect([
+            1 => 5,
+            2 => 3,
+            3 => 7,
+        ]);
+
+        factory(Category::class, $categoriesTransactions->count())->create();
+        $wallet = factory(Wallet::class)->create();
+        $categoriesTransactions->each(function ($category_id, $count) use ($wallet) {
+            factory(Transaction::class,$count)->create([
+                'trackable_type' => Wallet::class,
+                'trackable_id' => $wallet->id,
+                'category_id' => $category_id,
+            ]);
+        });
+
+        $response = $this->get('api/categories?sort=usage');
+        $response->assertSuccessful();
+
+        $body = json_decode($response->content());
+        $this->assertEquals(3,$body[0]->id);
+        $this->assertEquals(1,$body[1]->id);
+        $this->assertEquals(2,$body[2]->id);
     }
 }
