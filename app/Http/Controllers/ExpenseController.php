60ed3ff14baf5e59755bbbf50175bed2bd7c4069
Mohammed S Shurrab <mshurrab@extreme.sa>
2020-01-26 13:29:19 +0300

    Track daily expenses in a bulk request.

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transaction;
+use App\Wallet;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class ExpenseController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Collection
+     */
+    public function store(Request $request)
+    {
+        return collect($request->all())->map(function($expense){
+            $wallet = Wallet::find($expense['wallet_id']);
+            unset($expense['wallet_id']);
+            return $wallet->withdraw($expense);
+        });
+    }
+}
--- app/Http/Controllers/WalletExpenseController.php
+++ app/Http/Controllers/WalletExpenseController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Transaction;
 use App\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class WalletExpenseController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Wallet  $wallet
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Wallet $wallet
+     * @return Response
      */
     public function store(Request $request, Wallet $wallet)
     {
