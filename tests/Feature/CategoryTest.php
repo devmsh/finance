9bb8b0feb913fa570795bfbe77c8cd093c75ce4b
Mohammed S Shurrab <mshurrab@extreme.sa>
2020-01-26 12:37:15 +0300

    Introduce TrakcingTest feature file and move some tests to it

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -50,60 +50,6 @@ class CategoryTest extends TestCase
         $response->assertJsonCount(2);
     }
 
-    public function test_wallet_can_track_income_with_category()
-    {
-        $wallet = factory(Wallet::class)->create();
-
-        $category = factory(Category::class)->create([
-            'type' => Category::INCOME,
-        ]);
-
-        $response = $this->post("api/wallets/{$wallet->id}/income", [
-            'note' => 'Salary',
-            'amount' => 1000,
-            'category_id' => $category->id,
-        ]);
-
-        $response->assertSuccessful();
-        $response->assertJsonStructure([
-            'id',
-            'note',
-            'amount',
-        ]);
-
-        $transaction = Transaction::find(1);
-        $this->assertEquals('Salary', $transaction->note);
-        $this->assertEquals(1000, $transaction->amount);
-        $this->assertInstanceOf(Wallet::class, $transaction->trackable);
-    }
-
-    public function test_wallet_can_track_expenses_with_category()
-    {
-        $wallet = factory(Wallet::class)->create();
-
-        $category = factory(Category::class)->create([
-            'type' => Category::EXPENSES,
-        ]);
-
-        $response = $this->post("api/wallets/{$wallet->id}/expenses", [
-            'note' => 'Restaurant',
-            'amount' => 100,
-            'category_id' => $category->id,
-        ]);
-
-        $response->assertSuccessful();
-        $response->assertJsonStructure([
-            'id',
-            'note',
-            'amount',
-        ]);
-
-        $transaction = Transaction::find(1);
-        $this->assertEquals('Restaurant', $transaction->note);
-        $this->assertEquals(-100, $transaction->amount);
-        $this->assertInstanceOf(Wallet::class, $transaction->trackable);
-    }
-
     public function test_can_sort_categories_based_on_usage()
     {
         $categoriesTransactions = collect([
---
+++ tests/Feature/TrakcingTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Category;
+use App\Transaction;
+use App\Wallet;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TrakcingTest extends TestCase
+{
+    public function test_wallet_can_track_income_with_category()
+    {
+        $wallet = factory(Wallet::class)->create();
+
+        $category = factory(Category::class)->create([
+            'type' => Category::INCOME,
+        ]);
+
+        $response = $this->post("api/wallets/{$wallet->id}/income", [
+            'note' => 'Salary',
+            'amount' => 1000,
+            'category_id' => $category->id,
+        ]);
+
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'note',
+            'amount',
+        ]);
+
+        $transaction = Transaction::find(1);
+        $this->assertEquals('Salary', $transaction->note);
+        $this->assertEquals(1000, $transaction->amount);
+        $this->assertInstanceOf(Wallet::class, $transaction->trackable);
+    }
+
+    public function test_wallet_can_track_expenses_with_category()
+    {
+        $wallet = factory(Wallet::class)->create();
+
+        $category = factory(Category::class)->create([
+            'type' => Category::EXPENSES,
+        ]);
+
+        $response = $this->post("api/wallets/{$wallet->id}/expenses", [
+            'note' => 'Restaurant',
+            'amount' => 100,
+            'category_id' => $category->id,
+        ]);
+
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'id',
+            'note',
+            'amount',
+        ]);
+
+        $transaction = Transaction::find(1);
+        $this->assertEquals('Restaurant', $transaction->note);
+        $this->assertEquals(-100, $transaction->amount);
+        $this->assertInstanceOf(Wallet::class, $transaction->trackable);
+    }
+}
